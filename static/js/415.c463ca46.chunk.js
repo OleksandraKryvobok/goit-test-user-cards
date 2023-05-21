f39b09228b5aac78e79c503a35a4eb43919cda30
OleksandraKryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2023-05-21 17:22:26 +0000

    Deploying to gh-pages from @ OleksandraKryvobok/goit-test-user-cards@d1476786a37ec48792ae672323a94fe7909a87e0 🚀

--- static/js/415.c463ca46.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_test_user_cards=self.webpackChunkgoit_test_user_cards||[]).push([[415],{415:function(e,s,t){t.r(s);var r=t(184);s.default=function(){return(0,r.jsx)("h1",{children:"It`s a home page"})}}}]);
-//# sourceMappingURL=415.c463ca46.chunk.js.map
