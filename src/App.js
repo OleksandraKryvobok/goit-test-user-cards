fb8735433a641508e5a6d2d7b63b38331bf5f0a5
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2023-05-21 20:16:23 +0300

    add lazy import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Layout } from "./Layout";
 import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
-import Tweets from "./pages/Tweets";
 import { GlobalStyle } from "./GlobalStyle";
 
 const SharedLayout = lazy(() => import('./components/SharedLayout'));
 const Home = lazy(() => import('./pages/Home'));
+const Users = lazy(() => import('./pages/Users'));
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path='/'element={<SharedLayout />}>
             <Route index element={<Home />} />
-            <Route path='tweets' element={<Tweets />} />
+            <Route path='users' element={<Users />} />
             <Route path="*" element={<h1>NotFound</h1>} />
           </Route>
         </Routes>
